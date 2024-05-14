04949ceb79f50eef2cfcea9fcbf25d559cd5e70a
ManziPatrick <patrick.manzi@amalitech.org>
2024-05-14 15:20:16 +0200

    deployed version

--- dashboard/js/User.js
+++ dashboard/js/User.js
@@ -1,3 +1,4 @@
+
 const parseJwt = (token) => {
     try {
         return JSON.parse(atob(token.split('.')[1]));
@@ -7,43 +8,70 @@ const parseJwt = (token) => {
 };
 
 
+const token = localStorage.getItem('token');
 
+const redirectToLogin = () => {
+    window.location.href = '/sign_in.html'; 
+};
 
-var token = localStorage.getItem('token');
-if (token) {
-    var userInfo = parseJwt(token);
+if (!token) {
+    console.error('Token not found in localStorage');
+    redirectToLogin();
+} else {
+    const userInfo = parseJwt(token);
     if (userInfo) {
         console.log(userInfo.name);
-      
         const firstName = userInfo.name.split(' ')[0];
-        document.querySelector("#Profile-name").innerHTML=firstName;
+        document.querySelector("#Profile-name").innerHTML = firstName;
+        const userId = localStorage.getItem('userID');
+        console.log("userId ", userId)
+        console.log('Fetching user data for user ID:', userId);
+        fetch(`https://cyberops-bn.onrender.com/api/v1/user/getAdminById/${userId}`)
+            .then(response => {
+                console.log('Response status:', response.status);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch user data');
+                }
+                return response.json();
+            })
+            .then(userData => {
+                console.log('User data received:', userData);
+                console.log('Element:', document.querySelector('.img-circle'));
+                document.getElementById('navpic').src = userData.profilePic || 'plugins/images/large/img1.jpg';
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     } else {
         console.error('Invalid token:', token);
+        redirectToLogin(); 
     }
-} else {
-    console.error('Token not found in localStorage');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-   
-    const userId = localStorage.getItem('userID');
-console.log("userId ",userId )
-console.log('Fetching user data for user ID:', userId);
-fetch(`https://cyberops-bn.onrender.com/api/v1/user/getAdminById/${userId}`)
-    .then(response => {
-        console.log('Response status:', response.status);
-        if (!response.ok) {
-            throw new Error('Failed to fetch user data');
-        }
-        return response.json(); 
-    })
-    .then(userData => {
-        console.log('User data received:', userData);
-        console.log('Element:', document.querySelector('.img-circle'));
-        document.getElementById('navpic').src = userData.profilePic || 'plugins/images/large/img1.jpg';
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
 
-})
+document.addEventListener("DOMContentLoaded", function() {
+    var logoutBtn = document.getElementById('logoutBtn');
+    
+    logoutBtn.addEventListener('click', function() {
+      
+        localStorage.removeItem('token');
+       
+        window.location.href = '/sign_in.html'; 
+    });
+});
+
+
+const authenticatedRoutes = ['dashboard/dashboard.html', 'profile.html', 'DashbordUsers.html', 'newUser.html', 'map-google.html', 'DashboardBlog.html', 'appointment.html'];
+
+const currentPath = window.location.pathname;
+
+const requiresAuthentication = (route) => {
+    return authenticatedRoutes.includes(route);
+};
+
+if (requiresAuthentication(currentPath)) {
+  
+    if (!token) {
+        redirectToLogin();
+    }
+}
